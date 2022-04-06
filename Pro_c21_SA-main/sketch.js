0e26a2cf26e03c255f7bb8d05581f7532dfdcab3
Ibada22 <97994825+Ibada22@users.noreply.github.com>
2022-04-06 19:44:51 -0400

    Add files via upload

---
+++ Pro_c21_SA-main/sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  bb1=createImg ('right.png')
+  bb1.position (220,30);
+  bb1.size(50,50)
+  bb1.mouseClicked(hforce)
+  bb2=createImg ('up.png')
+  bb2.position (20,30)
+  bb2.size (50,50)
+  bb2.mouseClicked(vforce)
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ var ball_option = {
+   restitution: 0.9
+ }
+ ball=Bodies.circle (200,110,20,ball_option);
+ World.add (world,ball)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+  ellipse (ball.position.x,ball.position.y,20)
+}
+function hforce (){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0})
+}
+function vforce (){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+}
